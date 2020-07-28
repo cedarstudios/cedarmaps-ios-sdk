e383009b7a98e70efcd4fc54d77651b4bf5d0a57
Saeed Taheri <saeed.taheri@gmail.com>
2020-07-28 15:33:33 +0430

    Fixes a memory leak in CSMapView.

--- CedarMaps.podspec
+++ CedarMaps.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CedarMaps'
-  s.version          = '3.4.0'
+  s.version          = '3.4.1'
   s.summary          = 'CedarMaps iOS SDK'
 
 # This description is used to generate tags and improve search results.
--- CedarMaps/Classes/CSMapView.m
+++ CedarMaps/Classes/CSMapView.m
@@ -23,126 +23,127 @@
 
 - (instancetype)init
 {
-    self = [super init];
-    if (self) {
-        [self initialSetup];
-    }
-    return self;
+	self = [super init];
+	if (self) {
+		[self initialSetup];
+	}
+	return self;
 }
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    if (self) {
-        [self initialSetup];
-    }
-    return self;
+	self = [super initWithCoder:aDecoder];
+	if (self) {
+		[self initialSetup];
+	}
+	return self;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        [self initialSetup];
-    }
-    return self;
+	self = [super initWithFrame:frame];
+	if (self) {
+		[self initialSetup];
+	}
+	return self;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame styleURL:(NSURL *)styleURL {
-    self = [super initWithFrame:frame styleURL:styleURL];
-    if (self) {
-        [self initialSetup];
-    }
-    return self;
+	self = [super initWithFrame:frame styleURL:styleURL];
+	if (self) {
+		[self initialSetup];
+	}
+	return self;
 }
 
 - (void)initialSetup {
-    NSBundle *bundle = [self assetsBundle];
-    [self.logoView setImage:[UIImage imageNamed:@"cedarmaps" inBundle:bundle compatibleWithTraitCollection:nil]];
+	NSBundle *bundle = [self assetsBundle];
+	[self.logoView setImage:[UIImage imageNamed:@"cedarmaps" inBundle:bundle compatibleWithTraitCollection:nil]];
 	[self.logoView setContentMode:UIViewContentModeScaleAspectFit];
-    self.attributionButton.alpha = 0;
-    self.layer.backgroundColor = [UIColor colorWithRed: 249.0/255.0 green: 245.0/255.0 blue: 237.0/255.0 alpha: 1.0].CGColor;
-    
-    self.localObserver = [[NSNotificationCenter defaultCenter] addObserverForName:kCedarMapsAccessTokenIsReadeyNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
-        NSString *token = [note.userInfo objectForKey:kCedarMapsAccessTokenIsReadeyNotification];
-        [MGLAccountManager setAccessToken:token];
-        
-        if (!self.currentStyle) {
-            [self setDefaultStyleURL];
-        } else {
-            [self setStyleURL:[NSURL URLWithString:self.currentStyle]];
-        }
-    }];
-    
-    if ([[CSAuthenticationManager sharedAuthenticationManager] isAccessTokenSaved]) {
-        [self setDefaultStyleURL];
-    }
-    
-    [self setNeedsLayout];
+	self.attributionButton.alpha = 0;
+	self.layer.backgroundColor = [UIColor colorWithRed: 249.0/255.0 green: 245.0/255.0 blue: 237.0/255.0 alpha: 1.0].CGColor;
+
+	__weak CSMapView *weakSelf = self;
+	self.localObserver = [[NSNotificationCenter defaultCenter] addObserverForName:kCedarMapsAccessTokenIsReadeyNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+		NSString *token = [note.userInfo objectForKey:kCedarMapsAccessTokenIsReadeyNotification];
+		[MGLAccountManager setAccessToken:token];
+
+		if (!weakSelf.currentStyle) {
+			[weakSelf setDefaultStyleURL];
+		} else {
+			[weakSelf setStyleURL:[NSURL URLWithString:weakSelf.currentStyle]];
+		}
+	}];
+
+	if ([[CSAuthenticationManager sharedAuthenticationManager] isAccessTokenSaved]) {
+		[self setDefaultStyleURL];
+	}
+
+	[self setNeedsLayout];
 }
 
 - (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:_localObserver];
+	[[NSNotificationCenter defaultCenter] removeObserver:_localObserver];
 }
 
 - (void)setStyleURL:(NSURL *)styleURL {
-    if (!styleURL) {
-        return;
-    }
-    if (!self.currentStyle && [self.currentStyle isEqualToString:styleURL.absoluteString]) {
-        return;
-    }
-    
-    self.currentStyle = styleURL.absoluteString;
-    if ([styleURL.absoluteString containsString:@"access_token"]) {
-        [super setStyleURL:styleURL];
-    } else {
-        [[CSAuthenticationManager sharedAuthenticationManager] accessToken:^(NSString * _Nullable token, NSError * _Nullable error) {
-            if (token) {
-                NSString *urlStr = [NSString stringWithFormat:@"%@?access_token=%@", styleURL.absoluteString, token];
-                NSString *encodedURLStr = [urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
-                [super setStyleURL:[NSURL URLWithString:encodedURLStr]];
-            }
-        }];
-    }
+	if (!styleURL) {
+		return;
+	}
+	if (!self.currentStyle && [self.currentStyle isEqualToString:styleURL.absoluteString]) {
+		return;
+	}
+
+	self.currentStyle = styleURL.absoluteString;
+	if ([styleURL.absoluteString containsString:@"access_token"]) {
+		[super setStyleURL:styleURL];
+	} else {
+		[[CSAuthenticationManager sharedAuthenticationManager] accessToken:^(NSString * _Nullable token, NSError * _Nullable error) {
+			if (token) {
+				NSString *urlStr = [NSString stringWithFormat:@"%@?access_token=%@", styleURL.absoluteString, token];
+				NSString *encodedURLStr = [urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+				[super setStyleURL:[NSURL URLWithString:encodedURLStr]];
+			}
+		}];
+	}
 }
 
 - (void)setDefaultStyleURL {
-    __weak CSMapView *weakSelf = self;
-    [[CSAuthenticationManager sharedAuthenticationManager] accessToken:^(NSString * _Nullable token, NSError * _Nullable error) {
-        if (token) {
-            NSString *urlStr = [NSString stringWithFormat:@"%@styles/cedarmaps.light.json?access_token=%@", [[CSAuthenticationManager sharedAuthenticationManager] baseURL], token];
-            NSString *encodedURLStr = [urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
-            [weakSelf setStyleURL:[NSURL URLWithString:encodedURLStr]];
-        }
-    }];
+	__weak CSMapView *weakSelf = self;
+	[[CSAuthenticationManager sharedAuthenticationManager] accessToken:^(NSString * _Nullable token, NSError * _Nullable error) {
+		if (token) {
+			NSString *urlStr = [NSString stringWithFormat:@"%@styles/cedarmaps.light.json?access_token=%@", [[CSAuthenticationManager sharedAuthenticationManager] baseURL], token];
+			NSString *encodedURLStr = [urlStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+			[weakSelf setStyleURL:[NSURL URLWithString:encodedURLStr]];
+		}
+	}];
 }
 
 - (MGLAnnotationImage *)defaultAnnotationImage
 {
-    MGLAnnotationImage *annotationImage = [super defaultAnnotationImage];
-    
-    NSBundle *bundle = [self assetsBundle];
-    if (bundle) {
-        UIImage *image = [UIImage imageNamed:@"cedarmaps_default" inBundle:bundle compatibleWithTraitCollection:nil];
-        image = [image imageWithAlignmentRectInsets:
-                 UIEdgeInsetsMake(0, 0, image.size.height / 2, 0)];
-        
-        if (image) {
-            [annotationImage setImage:image];
-        }
-    }
+	MGLAnnotationImage *annotationImage = [super defaultAnnotationImage];
 
-    return annotationImage;
+	NSBundle *bundle = [self assetsBundle];
+	if (bundle) {
+		UIImage *image = [UIImage imageNamed:@"cedarmaps_default" inBundle:bundle compatibleWithTraitCollection:nil];
+		image = [image imageWithAlignmentRectInsets:
+				 UIEdgeInsetsMake(0, 0, image.size.height / 2, 0)];
+
+		if (image) {
+			[annotationImage setImage:image];
+		}
+	}
+
+	return annotationImage;
 }
 
 - (NSBundle *)assetsBundle {
-    NSBundle *podBundle = [NSBundle bundleForClass:[self classForCoder]];
-    NSURL *bundleURL = [podBundle URLForResource:@"Assets" withExtension:@"bundle"];
-    if (bundleURL) {
-        NSBundle *bundle = [NSBundle bundleWithURL:bundleURL];
-        return bundle;
-    }
-    return nil;
+	NSBundle *podBundle = [NSBundle bundleForClass:[self classForCoder]];
+	NSURL *bundleURL = [podBundle URLForResource:@"Assets" withExtension:@"bundle"];
+	if (bundleURL) {
+		NSBundle *bundle = [NSBundle bundleWithURL:bundleURL];
+		return bundle;
+	}
+	return nil;
 }
 
 @end
